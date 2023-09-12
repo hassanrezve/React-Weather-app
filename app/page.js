b2074d07db74c38ad40706a74ea829efd6b8aba3
hassanrezve <rezveshah1211@gmail.com>
2023-09-12 02:20:38 -0700

    1st commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,110 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Home() {
+const Page = () => {
+  const [winput, setwinput] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [container, setcontainer] = useState("105");
+  const [errorclasses, seterrorclasses] = useState("");
+  const APIKey = 'c4f983eeacd7b8775371fc019f8d0cdd';
+  const getIcon=()=>
+  {
+    switch (weatherData.weather[0].main) {
+      case 'Clear':
+         return 'images/clear.png';
+          break;
+
+      case 'Rain':
+         return 'images/rain.png';
+          break;
+
+      case 'Snow':
+         return 'images/snow.png';
+          break;
+
+      case 'Clouds':
+         return 'images/cloud.png';
+          break;
+
+      case 'Haze':
+         return 'images/mist.png';
+          break;
+
+      default:
+         return '';
+  }
+  }
+  const fetchWeatherData = async () => {
+   
+    if (winput === '') return;
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${winput}&appid=${APIKey}`
+      );
+      setWeatherData(response.data);
+      console.log(response.data);
+      setError(null);
+      setcontainer("605")
+    } catch (error) {
+      setWeatherData(null);
+      seterrorclasses("d-block fadeIn");
+      setError('OPS invalid Location 🤷‍♂️');
+      setcontainer("350")
+    }
+    
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="container" style={{ height: `${container}px` }}>
+        <div className="search-bar">
+          <i className="fa fa-map-marker"></i>
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            onChange={(e) => setwinput(e.target.value)}
+          ></input>
+          <button className="fa fa-search" onClick={fetchWeatherData}></button>
         </div>
+        
+          <>
+        <div className={`not-found${errorclasses}`}>
+          {error && 
+          ( <> <img className='not-found-img' src="/not-found.png" alt="Not Found"></img>
+          <p className='not-foundp'>{error}</p></> )}
+              </div>
+            </>
+        
+      
+         
+        {weatherData && (
+          <>
+            <div className="weather-box fadeIn">
+              <img className='weather-box-img' src={getIcon()} alt="Weather Icon"></img>
+              
+              <p className="temperature">{Math.round((weatherData.main.temp - 273.15))}°C</p>
+              <p className="description">{weatherData.weather.description}</p>
+            </div>
+            <div className="weather-details fadeIn">
+              <div className="humidity">
+                <i className="fa fa-tint weather-details-i"></i>
+                <span className='weather-details-span'>{weatherData.main.humidity}%</span>
+                <p className='weather-details-p'>Humidity</p>
+              </div>
+              <div className="wind">
+                <i className="fa fa-leaf weather-details-i"></i>
+                <span className='weather-details-span'>{weatherData.wind.speed}Km/h</span>
+                <p className='weather-details-p'>Wind Speed</p>
+              </div>
+            </div>
+          </>
+        )}
       </div>
+    </>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
